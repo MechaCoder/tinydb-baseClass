01fa18f87e67ed2c727761c7e2e8aa70ed8e373b
Matthew Proudman <proudmandandc@gmail.com>
2020-03-21 16:55:40 +0000

    adding tests to tests the data interg

--- test_tinyDbBase.py
+++ test_tinyDbBase.py
@@ -4,6 +4,7 @@ from tests.testData import TestData
 from tests.testOutput import TestExport
 from tests.testCryptography import TestFernetFactory, TestDatabaseBaseSercure
 from tests.testGetSet import TestGetSet
+from tests.testDataFile import TestDataFile
 
 if __name__ == '__main__':
     main()
