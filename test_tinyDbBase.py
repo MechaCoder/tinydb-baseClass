14f46f57f4be9a0adfaface62c2c0f377ea07ecc
Matthew Proudman <proudmandandc@gmail.com>
2020-03-25 18:58:31 +0000

    ::added unit tests

--- test_tinyDbBase.py
+++ test_tinyDbBase.py
@@ -5,6 +5,7 @@ from tests.testOutput import TestExport
 from tests.testCryptography import TestFernetFactory, TestDatabaseBaseSercure
 from tests.testGetSet import TestGetSet
 from tests.testDataFile import TestDataFile
+from tests.testExporter import TestExporter
 
 if __name__ == '__main__':
     main()
---
+++ tests/testExporter.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+from os.path import exists 
+from time import time_ns
+from tinydb_base.exporter import jsonExport, ymalExport
+from tinydb_base import DatabaseBase
+
+class TestDB(DatabaseBase):
+
+    def __init__(self, file='ds.test.json', table=__name__, requiredKeys=['title']):
+        super().__init__(file=file, table=table, requiredKeys=requiredKeys)
+
+class TestExporter(TestCase):
+
+    def setUp(self):
+        self.fileStamp = "data_{}".format(time_ns())
+
+        for title in range(0,90):
+            TestDB().create({'title': title})
+        
+        self.data = TestDB().readAll()
+        return super().setUp()
+
+    def testJsonExport(self):
+
+        o = jsonExport(
+            self.data,
+            self.fileStamp + '.json'
+        )
+
+        self.assertIsInstance(o, str)
+
+    def testYmalExport(self):
+
+        o = ymalExport(
+            self.data,
+            self.fileStamp + '.ymal'
+        )
+
+        self.assertIsInstance(o, str)
